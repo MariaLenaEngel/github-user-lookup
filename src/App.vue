41755898ccf73dc450115f76f44c54136a3ca5d5
Maria <marialena@lakoer.de>
2022-07-14 12:39:01 +0200

    Initital commit.

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="wrapper">
+    <div class="autocomplete">
+      <input
+        class="searchInput"
+        type="text"
+        placeholder="Start typing a username"
+      />
+    </div>
+  </div>
+</template>
+
+<style></style>
